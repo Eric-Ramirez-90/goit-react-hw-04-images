4c45ea21ac03850bd08ed43a4fae8a0df1e8958d
Eric Ramirez <ramirez1990rer@gmail.com>
2023-02-12 20:13:41 +0200

    Update ImageGalleryItem with hooks

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,41 +1,33 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from '../Modal';
 import { Image } from './ImageGalleryItem.styled';
 
-class ImageGalleryItem extends Component {
-  static propTypes = {
-    item: PropTypes.shape({
-      webformatURL: PropTypes.string.isRequired,
-      tags: PropTypes.string.isRequired,
-      largeImageURL: PropTypes.string.isRequired,
-    }),
-  };
-  state = {
-    showModal: false,
+function ImageGalleryItem({ image: { webformatURL, tags, largeImageURL } }) {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(showModal => !showModal);
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
-  };
-
-  render() {
-    const { webformatURL, tags, largeImageURL } = this.props.image;
-    const { showModal } = this.state;
-
-    return (
-      <>
-        <Image src={webformatURL} alt={tags} onClick={this.toggleModal} />
-        {showModal && (
-          <Modal onClose={this.toggleModal}>
-            <img src={largeImageURL} alt={tags}></img>
-          </Modal>
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      <Image src={webformatURL} alt={tags} onClick={toggleModal} />
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <img src={largeImageURL} alt={tags}></img>
+        </Modal>
+      )}
+    </>
+  );
 }
+
+ImageGalleryItem.propTypes = {
+  image: PropTypes.shape({
+    webformatURL: PropTypes.string.isRequired,
+    tags: PropTypes.string.isRequired,
+    largeImageURL: PropTypes.string.isRequired,
+  }),
+};
 
 export default ImageGalleryItem;
